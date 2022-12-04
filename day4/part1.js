9e5eff0978cbf67633e55d0549d2a3c0e6dc71b2
Simon Waton <watsonsemon@gmail.com>
2022-12-05 00:03:18 +1300

    day4

---
+++ day4/part1.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const file = fs.readFileSync("./data.txt").toString().split("\r\n");
+
+let total = 0;
+
+for (const row of file) {
+	const pairs = row.split(",");
+	const range1 = pairs[0].split("-").map((x) => parseInt(x));
+	const range2 = pairs[1].split("-").map((x) => parseInt(x));
+
+	if ((range1[0] >= range2[0] && range1[1] <= range2[1]) || (range1[0] <= range2[0] && range1[1] >= range2[1])) {
+		total++;
+	}
+}
+
+console.log(total);

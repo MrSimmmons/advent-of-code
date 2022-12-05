506dd7ab5c03835c3e4963ab4012ba5a2005f69d
Simon Waton <watsonsemon@gmail.com>
2022-12-05 22:58:02 +1300

    chore: lint

--- day5/part1.js
+++ day5/part1.js
@@ -1,104 +1,41 @@
 const fs = require("fs");
 
-const file = fs.readFileSync("./moves.txt").toString().split(process.platform === "win32" ? "\r\n" : "\n");
+const file = fs
+	.readFileSync("./moves.txt")
+	.toString()
+	.split(process.platform === "win32" ? "\r\n" : "\n");
 
-let string = '';
+let string = "";
 
 let columns = [
-    [
-        'T',
-        'V',
-        'J',
-        'W',
-        'N',
-        'R',
-        'M',
-        'S'
-    ],
-    [
-        'V',
-        'C',
-        'P',
-        'Q',
-        'J',
-        'D',
-        'W',
-        'B'
-    ],
-    [
-        'P',
-        'R',
-        'D',
-        'H',
-        'F',
-        'J',
-        'B'
-    ],
-    [
-        'D',
-        'N',
-        'M',
-        'B',
-        'P',
-        'R',
-        'F'
-    ],
-    [
-        'B',
-        'T',
-        'P',
-        'R',
-        'V',
-        'H'
-    ],
-    [
-        'T',
-        'P',
-        'B',
-        'C'
-    ],
-    [
-        'L',
-        'P',
-        'R',
-        'J',
-        'B'
-    ],
-    [
-        'W',
-        'B',
-        'Z',
-        'T',
-        'L',
-        'S',
-        'C',
-        'N'
-    ],
-    [
-        'G',
-        'S',
-        'L'
-    ]
+	["T", "V", "J", "W", "N", "R", "M", "S"],
+	["V", "C", "P", "Q", "J", "D", "W", "B"],
+	["P", "R", "D", "H", "F", "J", "B"],
+	["D", "N", "M", "B", "P", "R", "F"],
+	["B", "T", "P", "R", "V", "H"],
+	["T", "P", "B", "C"],
+	["L", "P", "R", "J", "B"],
+	["W", "B", "Z", "T", "L", "S", "C", "N"],
+	["G", "S", "L"]
 ];
 
-columns.map(x => x.reverse());
+columns.map((x) => x.reverse());
 
 for (const row of file) {
-    // const numbers = row.match(/\d/g).map(x => parseInt(x)); // Doesnt work for 2 digit numbers
+	// const numbers = row.match(/\d/g).map(x => parseInt(x)); // Doesnt work for 2 digit numbers
 
-    const split = row.split(' ');
-    const numbers = [split[1], split[3], split[5]].map(x => parseInt(x));
+	const split = row.split(" ");
+	const numbers = [split[1], split[3], split[5]].map((x) => parseInt(x));
 
-    console.log(numbers);
+	console.log(numbers);
 
-    for (let i = 0; i < numbers[0]; i++) {
-        columns[numbers[2] - 1].push(columns[numbers[1] - 1].pop())
-    }
-
+	for (let i = 0; i < numbers[0]; i++) {
+		columns[numbers[2] - 1].push(columns[numbers[1] - 1].pop());
+	}
 }
 
 for (const column of columns) {
-    string += column[column.length - 1];
+	string += column[column.length - 1];
 }
 
 console.log(string);
--- day5/part2.js
+++ day5/part2.js
@@ -1,109 +1,45 @@
 const fs = require("fs");
 
-const file = fs.readFileSync("./moves.txt").toString().split(process.platform === "win32" ? "\r\n" : "\n");
+const file = fs
+	.readFileSync("./moves.txt")
+	.toString()
+	.split(process.platform === "win32" ? "\r\n" : "\n");
 
-let string = '';
+let string = "";
 
 let columns = [
-    [
-        'T',
-        'V',
-        'J',
-        'W',
-        'N',
-        'R',
-        'M',
-        'S'
-    ],
-    [
-        'V',
-        'C',
-        'P',
-        'Q',
-        'J',
-        'D',
-        'W',
-        'B'
-    ],
-    [
-        'P',
-        'R',
-        'D',
-        'H',
-        'F',
-        'J',
-        'B'
-    ],
-    [
-        'D',
-        'N',
-        'M',
-        'B',
-        'P',
-        'R',
-        'F'
-    ],
-    [
-        'B',
-        'T',
-        'P',
-        'R',
-        'V',
-        'H'
-    ],
-    [
-        'T',
-        'P',
-        'B',
-        'C'
-    ],
-    [
-        'L',
-        'P',
-        'R',
-        'J',
-        'B'
-    ],
-    [
-        'W',
-        'B',
-        'Z',
-        'T',
-        'L',
-        'S',
-        'C',
-        'N'
-    ],
-    [
-        'G',
-        'S',
-        'L'
-    ]
+	["T", "V", "J", "W", "N", "R", "M", "S"],
+	["V", "C", "P", "Q", "J", "D", "W", "B"],
+	["P", "R", "D", "H", "F", "J", "B"],
+	["D", "N", "M", "B", "P", "R", "F"],
+	["B", "T", "P", "R", "V", "H"],
+	["T", "P", "B", "C"],
+	["L", "P", "R", "J", "B"],
+	["W", "B", "Z", "T", "L", "S", "C", "N"],
+	["G", "S", "L"]
 ];
 
-columns.map(x => x.reverse());
+columns.map((x) => x.reverse());
 
 for (const row of file) {
+	const split = row.split(" ");
+	const numbers = [split[1], split[3], split[5]].map((x) => parseInt(x));
 
-    const split = row.split(' ');
-    const numbers = [split[1], split[3], split[5]].map(x => parseInt(x));
+	console.log(numbers);
 
-    console.log(numbers);
+	let stack = [];
 
-    let stack = [];
+	for (let i = 0; i < numbers[0]; i++) {
+		stack.push(columns[numbers[1] - 1].pop());
+	}
 
-    for (let i = 0; i < numbers[0]; i++) {
-        stack.push(columns[numbers[1] - 1].pop())
-    }
+	stack.reverse();
 
-    stack.reverse();
-
-    columns[numbers[2] - 1].push(...stack);
-
+	columns[numbers[2] - 1].push(...stack);
 }
 
 for (const column of columns) {
-    string += column[column.length - 1];
+	string += column[column.length - 1];
 }
 
 console.log(string);

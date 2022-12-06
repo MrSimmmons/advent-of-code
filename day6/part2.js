ef6cef28cbcf17009f040942c652e1e88ce4c34f
Simon Waton <watsonsemon@gmail.com>
2022-12-06 22:24:12 +1300

    feat: day6

---
+++ day6/part2.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const file = fs
+	.readFileSync("./data.txt")
+	.toString()
+	.split(process.platform === "win32" ? "\r\n" : "\n")[0];
+
+let string = "";
+
+for (let i = 0; i < file.length; i++) {
+	const character = file[i];
+	if (string.length > 13) {
+		if ([...new Set(string.split(""))].length === string.length) {
+			console.log(i);
+			break;
+		} else {
+			string = string.slice(1);
+		}
+	}
+
+	string += character;
+}
